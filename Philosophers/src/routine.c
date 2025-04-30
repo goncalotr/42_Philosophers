3348f0bbffa3919fc8d96e4c435b316ab315d1ab
goncalotr <goncalotribeiro@gmail.com>
2025-05-01 00:18:43 +0100

    feat(routine): main routine loop

--- Philosophers/src/routine.c
+++ Philosophers/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: goteixei <goteixei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 12:47:58 by goteixei          #+#    #+#             */
-/*   Updated: 2025/05/01 00:12:38 by goteixei         ###   ########.fr       */
+/*   Updated: 2025/05/01 00:16:28 by goteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,14 @@ void	*philo_routine(void *arg)
 		ft_usleep(1);
 	while (!philo_is_sim_over(philo))
 	{
-		
+		philo_take_forks_ordered(philo);
+		if (philo_is_sim_over(philo) || philo->num_of_philos == 1)
+		{
+			break;
+		}
+		philo_eat(philo);
+		philo_sleep(philo);
+		philo_think(philo);
 	}
 	return (NULL);
 }
--- Philosophers/src/routine_utils.c
+++ Philosophers/src/routine_utils.c
@@ -6,18 +6,23 @@
 /*   By: goteixei <goteixei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 19:10:31 by goteixei          #+#    #+#             */
-/*   Updated: 2025/04/18 19:15:53 by goteixei         ###   ########.fr       */
+/*   Updated: 2025/05/01 00:17:53 by goteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-int	philo_simulation_over(t_philo *philo)
+/**
+ * @brief Safely checks if the simulation has ended.
+ * @param philo Pointer to the philosopher's structure.
+ * @return 1 if simulation is over (death flag set), 0 otherwise.
+ */
+int	philo_is_sim_over(t_philo *data)
 {
 	int	result;
-	
-	pthread_mutex_lock(philo->dead_lock);
-	result = *(philo->dead);
-	pthread_mutex_unlock(philo->dead_lock);
+
+	pthread_mutex_lock(data->dead_lock);
+	result = *(data->dead);
+	pthread_mutex_unlock(data->dead_lock);
 	return (result);
 }
