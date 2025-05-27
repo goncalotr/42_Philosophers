3e509d54118ba7035f7da61ace7cc46f7ba10c5a
goncalotr <goncalotribeiro@gmail.com>
2025-05-27 15:22:28 +0100

    fix(arguments): check for negative arguments

--- Philosophers/src/init.c
+++ Philosophers/src/init.c
@@ -6,7 +6,7 @@
 /*   By: goteixei <goteixei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 10:29:17 by goteixei          #+#    #+#             */
-/*   Updated: 2025/05/27 13:39:50 by goteixei         ###   ########.fr       */
+/*   Updated: 2025/05/27 15:19:27 by goteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,12 @@ int	philo_check_valid_args(int argc, char **argv)
 			return (philo_error_msg("Number of philosophers must be > 0."));
 		if (temp_val > INT_MAX)
 			return (philo_error_msg("Argument value too large."));
-		if (i == 1 && temp_val > PHILO_MAX)
+		if (i == 1 && (temp_val > PHILO_MAX || temp_val <= 0))
 			return \
-	(philo_error_msg("Number of philosophers exceeds PHILO_MAX."));
+	(philo_error_msg("Number of philosophers > PHILO_MAX or <= 0."));
+		if (i >= 2 && temp_val < 0)
+			return \
+	(philo_error_msg("Time arguments and meal count must be non-negative."));
 		i++;
 	}
 	return (0);
