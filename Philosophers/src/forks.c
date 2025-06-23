275ca819c16347fba908b0d6f9e967385018942c
goncalotr <goncalotribeiro@gmail.com>
2025-06-23 18:50:54 +0100

    fix(forks): remove unused forks ordering variables and function

--- Philosophers/src/forks.c
+++ Philosophers/src/forks.c
@@ -6,7 +6,7 @@
 /*   By: goteixei <goteixei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 00:38:57 by goteixei          #+#    #+#             */
-/*   Updated: 2025/06/23 18:36:30 by goteixei         ###   ########.fr       */
+/*   Updated: 2025/06/23 18:38:53 by goteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,35 +22,6 @@ void	philo_release_forks(t_philo *philo)
 	if (philo->num_of_philos > 1)
 		pthread_mutex_unlock(philo->fork_b);
 }
-
-/**
- * @brief Determines the order of forks to pick up for deadlock prevention
- *        and assigns them to the provided pointer-to-pointer arguments.
- * @param philo Pointer to the philosopher's structure.
- * @param first_fork_ptr Pointer to a pthread_mutex_t pointer, will be set
- *                       to the first fork to lock.
- * @param second_fork_ptr Pointer to a pthread_mutex_t pointer, will be set
- *                        to the second fork to lock.
- * 
- * Alternative to if (philo->id % 2 == 0)
- * if (philo->fork_a < philo->fork_b)
- */
-/*
-static void	philo_take_forks_aux(t_philo *philo, \
-pthread_mutex_t	**first_fork_ptr, pthread_mutex_t	**second_fork_ptr)
-{
-	if (philo->id % 2 == 0)
-	{
-		*first_fork_ptr = philo->fork_a;
-		*second_fork_ptr = philo->fork_b;
-	}
-	else
-	{
-		*first_fork_ptr = philo->fork_b;
-		*second_fork_ptr = philo->fork_a;
-	}
-}
-*/
 
 /**
  * @brief Attempts to acquire both left and right forks using an ordered
@@ -74,10 +45,6 @@ pthread_mutex_t	**first_fork_ptr, pthread_mutex_t	**second_fork_ptr)
  */
 int	philo_take_forks_ordered(t_philo *philo)
 {
-	//pthread_mutex_t	*first_fork;
-	//pthread_mutex_t	*second_fork;
-
-	//philo_take_forks_aux(philo, &first_fork, &second_fork);
 	if (philo_is_sim_over(philo))
 		return (1);
 	pthread_mutex_lock(philo->fork_a);
